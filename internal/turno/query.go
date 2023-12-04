e91c1de9f103cb151e9ef54929a9aae9c0daab27
jum8 <jumeight@gmail.com>
2023-12-04 06:59:57 -0300

    fix: saco nombre de base de datos de query delete turno

--- internal/turno/query.go
+++ internal/turno/query.go
@@ -4,7 +4,7 @@ var (
 	QuertyInsertTurno           = `INSERT INTO turno (fecha_hora, descripcion, odontologo_id, paciente_id) VALUES (?,?,?,?)`
 	QueryGetTurnoById           = `SELECT id, fecha_hora, descripcion, odontologo_id, paciente_id FROM turno WHERE id = ?`
 	QueryUpdateTurno            = `UPDATE turno SET fecha_hora = ?, descripcion = ?, odontologo_id = ?, paciente_id = ? WHERE id = ?`
-	QueryDeleteTurno            = `DELETE FROM clinica_dental.turno WHERE id = ?;`
+	QueryDeleteTurno            = `DELETE FROM turno WHERE id = ?;`
 	QueryGetTurnosByPacienteDNI = `SELECT turno.id, turno.fecha_hora, turno.descripcion,
 	odontologo.*, paciente.*
 	FROM turno
